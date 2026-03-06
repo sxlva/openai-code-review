6d8eb6f4e5bf1f5a1e124e901c7d90f8ba5541a0
SXLVA <3422234609@qq.com>
2026-03-06 11:04:28 +0800

    v1

--- openai-code-review-test/src/test/java/cn/fcr/middleware/test/ApiTest.java
+++ openai-code-review-test/src/test/java/cn/fcr/middleware/test/ApiTest.java
@@ -18,7 +18,12 @@ public class ApiTest {
 
     @Test
     public void test() {
-        System.out.println(Integer.parseInt("aaaa"));
+
+        System.out.println(Integer.parseInt("aaaa1"));
+        System.out.println(Integer.parseInt("aaaa2"));
+        System.out.println(Integer.parseInt("aaaa3"));
+        System.out.println(Integer.parseInt("aaaa4"));
+
     }
 
 }
